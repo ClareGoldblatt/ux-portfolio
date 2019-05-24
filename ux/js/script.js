a188fdf9e9510324ed8dc9c8e3c80761f65f29e7
Clare Goldblatt <claregoldblatt@gmail.com>
2019-05-24 13:35:54 +0200

    Add files via upload

---
+++ ux/js/script.js
@@ -0,0 +1,16 @@
+var textOverImages = document.getElementsByClassName('onclicktextoverimage')
+var previousTextOverImage
+for (var i = 0; i < textOverImages.length; i++) {
+  textOverImages[i].onclick = function () {
+    var classes = this.classList
+    if (classes.contains('show')) {
+      classes.remove('show')
+    } else {
+      if (previousTextOverImage != null) {
+        previousTextOverImage.classList.remove('show')
+        previousTextOverImage = this
+        classes.add('show')
+      }
+    }
+  }
+}
